4b429c726f620d6d0caa225af860a631143e4821
Gabriel OLiveira <gabrielerika8724@gmail.com>
2025-08-25 14:22:07 -0300

    primeiro

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const readline= require('readline');
+
+const rl = readline.createInterface({
+        input:process.stdin,
+        output: process.stdout
+
+
+});
+function pergunta(questao) {
+        return new Promise((resolve)=>{
+            rl.question(questao, (resposta)=> {
+                resolve(resposta);                         
+            });
+
+        })
+}
+
+async function perguntarIdade(){
+    const idadeStr = await pergunta("Qual é sua idade?");
+    const idade = parseInt(idadeStr);
+    
+        if(idade<18) {
+            console.log("Você é menor de idade");
+        } else if (idade > 60) {
+            console.log("Você é idoso");
+        } else {
+            console.log("Você é só um adulto");
+        }
+        rl.close();
+    }
+
+    perguntarIdade();
